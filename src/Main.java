10b644f8e8f26c9f0bb16a96f414bba206cb78b4
James <jamessnee1@gmail.com>
2016-08-09 14:56:09 +1200

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,48 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+		
+		int numRows = 0;
+		
+		Scanner s = new Scanner(System.in);
+		System.out.printf("Please enter amount of rows to generate: ");
+		numRows = s.nextInt();
+		System.out.printf("Please enter a username to generate: ");
+		String username = s.next();
+		
+		File file = new File("data.csv");
+		
+		//if file exists, create new file regardless
+		if(file.exists()){
+			file.createNewFile();
+			
+		}
+		
+		FileWriter fw = new FileWriter(file);
+		BufferedWriter bw = new BufferedWriter(fw);
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append(username);
+		
+		for(int i = 0; i < numRows; i++){
+			
+			//bw.write(sb.append(i + "\n").toString());
+			bw.write(sb.append(i + ",").toString());
+			bw.newLine();
+			sb.setLength(sb.length() - 2);
+			
+		}
+		
+		bw.close();
+		System.out.println("File data.csv written!\n");
+
+	}
+
+}
